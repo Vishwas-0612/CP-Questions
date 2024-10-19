7dd2acbc3e184acd4b01d9663e5fc6f79423e68a
Vishwas-0612 <srivastavavishwas01@gmail.com>
2024-10-19 20:07:01 +0530

    Comparison String

--- q1837B.cpp
+++ q1837B.cpp
@@ -5,27 +5,32 @@ int check(string s, int n)
 {
     vector<int> v(n + 1);
     unordered_map<int, int> m;
-
-    if (s[0] == '<')
+    int flag = 0;
+    v[0] = flag;
+    for (int i = 0; i < n; i++)
     {
-        v.push_back(1);
-    }
-    else
-    {
-        v.push_back(2);
-    }
-    for (int i = 1; i < n; i++)
-    {
-        if (s[i] == '>')
+        if (s[i] == '<')
         {
-            v[i] = v[i - 1] + 1;
+            flag += 1;
         }
         else
         {
-            v[i] = v[i - 1] - 1;
+            flag -= 1;
+        }
+        v[i + 1] = flag;
+    }
+    int cnt = 0;
+    for (int i = 0; i < n + 1; i++)
+    {
+        m[v[i]]++;
+        if (m[v[i]] == 1)
+        {
+            cnt++;
         }
     }
+    return cnt;
 }
+
 int main()
 {
     int t;
@@ -36,8 +41,9 @@ int main()
         int n;
         cin >> n;
         string s;
+        cin >> s;
 
-        int ans = (s, n);
+        int ans = check(s, n);
         cout << ans << endl;
     }
     return 0;
